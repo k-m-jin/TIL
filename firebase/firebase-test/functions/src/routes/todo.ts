381b8484cf8170d5bfb999f4b40cb5727a248e05
k-m-jin <mel917@naver.com>
2022-07-05 22:39:08 +0900

    220705 firebase-3

--- firebase/firebase-test/functions/src/routes/todo.ts
+++ firebase/firebase-test/functions/src/routes/todo.ts
@@ -6,11 +6,35 @@ const db = admin.firestore()
 const router = express.Router()
 
 interface Todo {
+  id?:string
   title: string
   done: boolean
   createdAt: string
   updatedAt: string
+  deleted: boolean
 }
+
+//Job
+async function addDeleted() {
+  const snaps = await db.collection('Todos').get()
+  
+  //snap 안에 docs 에 유사배열 이 있음
+  for ( const snap of snaps.docs ) {
+    snap.ref.update({
+      deleted: false
+    })
+  }
+  console.log('done!')
+
+
+  // //forEach 가 콜백의 비동기를 보장하지 않음
+  // snaps.forEach(async snap => {
+  //   await snap.ref.update({
+  //     deleted: false
+  //   })
+  // })
+}
+// addDeleted()
 
 // 투두 조회
 // http://localhost:5001/kdt2-test/us-central1/api/todo
@@ -20,13 +44,13 @@ router.get('/', async (req, res) => {
   console.log(req.params)
   console.log(req.query)
 
-  const snaps = await db.collection('Todos').get()
+  const snaps = await db.collection('Todos')
+  .where('deleted','!=',true)
+  .get()
   //디비쿼리문
   // .where('done','==','false')
-  
-  type ResponseTodo = Todo & { id: string}
 
-  const todos: ResponseTodo[] = []
+  const todos: Todo[] = []
   snaps.forEach(snap => {
     //필드 부분
     const fields = snap.data()
@@ -34,6 +58,13 @@ router.get('/', async (req, res) => {
       id: snap.id,
       ...fields as Todo
     })
+  })
+
+  
+  todos.sort((a,b) => {
+    const aTime: number = new Date(a.createdAt).getTime()
+    const bTime: number = new Date(a.createdAt).getTime()
+    return bTime - aTime
   })
 
   res.status(200).json(todos)
@@ -46,7 +77,8 @@ router.post('/', async (req,res)=> {
     title,
     done: false,
     createdAt: new Date().toISOString(),
-    updatedAt: new Date().toISOString()
+    updatedAt: new Date().toISOString(),
+    deleted: false
   }
   
   const ref = await db.collection('Todos').add(todo)
@@ -57,7 +89,55 @@ router.post('/', async (req,res)=> {
     ...todo
   })
 })
-// router.put('')
-// router.delete('')
+
+//투두 수정
+router.put('/:id', async (req, res) => {
+ const { title, done } = req.body
+ const { id } = req.params
+
+ //snap : 디비에서 찾은 정보
+ const snap = await db.collection('Todos').doc(id).get()
+//에외처리
+ if (!snap.exists) {
+  return res.status(404).json('존재하지 않는 정보입니다.')
+  }
+
+ const { createdAt } = snap.data() as Todo
+ const updatedAt = new Date().toDateString()
+
+ await snap.ref.update({
+  title,
+  done,
+  updatedAt
+ })
+ 
+ return res.status(200).json({
+  id: snap.id,
+  title,
+  done,
+  createdAt,
+  updatedAt
+ })
+})
+
+//투두 삭제
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params
+
+  const snap = await db.collection('Todos').doc(id).get()
+  //예외처리
+  if (!snap.exists) {
+    return res.status(404).json('존재하지 않는 정보입니다.')
+  }
+
+  // 디비에서 실제 삭제는 비추천
+  // await snap.ref.delete()
+  snap.ref.update({
+    deleted: true
+  })
+
+  res.status(200).json(true)
+  return
+})
 
 export default router
