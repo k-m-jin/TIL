0c10177f0c694f707460397e064203e11b84817a
k-m-jin <mel917@naver.com>
2022-08-13 00:26:40 +0900

    220812 백엔드 소통

--- react/second-app/src/App.js
+++ react/second-app/src/App.js
@@ -10,8 +10,8 @@ function App() {
         <UserList />
       </UserProvider> */}
 
-      {/* <Axiosprc /> */}
-      <Weather />
+      {/* <Axiosprc />
+      <Weather /> */}
     </div>
   )
 }
